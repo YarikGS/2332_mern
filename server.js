13a138d8c46c573e5711b91763d91e91f89b23e1
Yarik Husynets <yarikgs12@gmail.com>
2020-03-17 18:20:53 +0200

    cors test 2

--- server.js
+++ server.js
@@ -5,15 +5,9 @@ const mongoose = require('mongoose')
 const cors = require('cors');
 
 const app = express()
-
+app.use(cors())
 app.use(express.json({ extended: true }))
-app.use(
-  cors({
-    credentials: true,
-    origin: ["https://sheltered-headland-76340.herokuapp.com:3000"],
-    optionsSuccessStatus: 200
-  })
-);
+
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/gallery', require('./routes/gallery.routes'))
 
