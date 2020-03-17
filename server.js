45573f31d3b2f2e87cb0bf5e45fb21ceffc1bb46
Yarik Husynets <yarikgs12@gmail.com>
2020-03-17 18:27:50 +0200

    cors test 2

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const mongoose = require('mongoose')
 const cors = require('cors');
 
 const app = express()
-app.use(cors())
-app.use(express.json({ extended: true }))
 
+app.use(express.json({ extended: true }))
+app.use(
+  cors({
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    origin: ["*"],
+    "preflightContinue": true,
+    optionsSuccessStatus: 200
+  })
+);
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/gallery', require('./routes/gallery.routes'))
 
