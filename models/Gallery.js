c5c99ec350e1564f1b20bafcac1391eb083e6b46
Yarik Husynets <yarikgs12@gmail.com>
2020-04-19 01:12:34 +0300

    gallery model test

--- models/Gallery.js
+++ models/Gallery.js
@@ -1,9 +1,11 @@
-const {Schema, model} = require('mongoose')
+const {Schema, model, Types} = require('mongoose')
 
 const schema = new Schema({
 	url: {type: String, required: true},
 	caption: { type: String, required: true},
-	category: { type: String },
+	// category: { type: String },
+	category:  {type: Types.ObjectId, ref: 'Gallery_category'
+    },
 	type: { type: String, required: true},
 	date: { type: Date, default: Date.now}
 })
